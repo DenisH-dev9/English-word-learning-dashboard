744769c84a3638d1ab2cd5463190101778704e47
Denis Parhomchuk <0676899640den@gmail.com>
2021-02-07 21:45:44 +0200

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      id: 0,
+      word: "",
+      translate: "",
+      cards: []
+    }
+  }
+
+  updateInput(key, value) {
+    this.setState({
+      [key] : value
+    })
+  }
+
+  addCard() {
+    const newCard = {
+      id: 1 + this.state.id,
+      value: {
+        word: this.state.word.slice(),
+        translate: this.state.translate.slice(),
+        overturned: false
+      }
+    }
+    
+    this.setState({
+      id: newCard.id,
+      word: "",
+      translate: "",
+      cards: [...this.state.cards, newCard]
+    })
+  }
+
+  turnCard(id) {
+    const cards = [...this.state.cards];
+
+    let index = cards.findIndex((card) => {
+      return card.id === id;
+    });
+
+    cards[index].value.overturned = !cards[index].value.overturned;
+
+    this.updateInput("cards", cards);
+  }
+
+render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      
+      <h1 className="app-title">Дошка англійських слів</h1>
+      <div className="container">
+        <h2>Додати картку</h2>
+        <input 
+          type="text"
+          placeholder="Введіть слово"
+          value={this.state.word}
+          onChange={(e : ChangeEvent<HTMLInputElement>) => this.updateInput("word", e.target.value)}
+        />
+        <input 
+          type="text"
+          placeholder="Введіть переклад"
+          value={this.state.translate}
+          onChange={(e : ChangeEvent<HTMLInputElement>) => this.updateInput("translate", e.target.value)}
+        />
+        <button 
+          className="btn add-btn"
+          onClick={() => this.addCard()}
         >
-          Learn React
-        </a>
-      </header>
+          Додати
+        </button>
+        <div className="cardsBlock">
+          {this.state.cards.map(card => {
+            return(
+              <div
+                key={card.id}
+                className={"card " + (card.value.overturned ? "overturned" : "")}
+                onClick={() => this.turnCard(card.id)}
+              >
+                {card.value.overturned ? card.value.translate : card.value.word}
+              </div>
+            )
+          })}
+        </div>
+      </div>
+
     </div>
-  );
+    );
+  }
 }
 
 export default App;
